10e5d17b62e56c8dc04181320b12c71d10446322
Trejon McGee <trejon.mcgee@gmail.com>
2023-10-24 08:32:37 -0500

    Added Delete technician

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import(
     api_list_technicians,
     api_list_appointments,
+    api_delete_technician
 )
 
 urlpatterns = [
     path("technicians/", api_list_technicians, name="technicians"),
     path("appointments/", api_list_appointments, name="appointments"),
+    path("technicians/<int:pk>/", api_delete_technician, name="delete_technicians")
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -90,6 +90,17 @@ def api_list_technicians(request):
         )
 
 
+@require_http_methods(["DELETE"])
+def api_delete_technician(request,pk):
+    if request.method == "DELETE":
+        count, _= Technician.objects.filter(id=pk).delete()
+        return JsonResponse(
+            {
+                "deleted": count > 0
+            }
+        )
+
+
 @require_http_methods(["GET", "POST"])
 def api_list_appointments(request):
     if request.method == "GET":
